9e8dc7a51530615bd2c15da52fc8240b4c089b8a
alerebal <alerebal@gmail.com>
2021-01-19 08:15:07 +0000

    heroku address fixed

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,11 +1,12 @@
+import { environment } from 'src/environments/environment';
 
-export const baseUrl = 'https://git.heroku.com/alerebal-ecommerce-back.git';
-export const productsUrl = `${baseUrl}/products`;
-export const cartItemsUrl = `${baseUrl}/cartItems`;
-export const newUserUrl = `${baseUrl}/newUser`;
-export const logInUrl = `${baseUrl}/logIn`;
-export const getUserUrl = `${baseUrl}/user`;
-export const stripeUrl = `${baseUrl}/payment`;
-export const noUserUrl = `${baseUrl}/noUser`;
+export const baseUrl = environment.production ? 'https://alerebal-ecommerce-back.herokuapp.com/' : 'http://localhost:3100/';
+export const productsUrl = `${baseUrl}products`;
+export const cartItemsUrl = `${baseUrl}cartItems`;
+export const newUserUrl = `${baseUrl}newUser`;
+export const logInUrl = `${baseUrl}logIn`;
+export const getUserUrl = `${baseUrl}user`;
+export const stripeUrl = `${baseUrl}payment`;
+export const noUserUrl = `${baseUrl}noUser`;
 
 
